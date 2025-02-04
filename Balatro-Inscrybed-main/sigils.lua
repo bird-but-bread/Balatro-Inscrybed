04df0e4dd6c87abda7a1ad143a74109d5405518d
smg9000 <71861369+smg9000@users.noreply.github.com>
2025-02-04 12:09:17 -0600

    Update sigils.lua

--- Balatro-Inscrybed-main/sigils.lua
+++ Balatro-Inscrybed-main/sigils.lua
@@ -360,18 +360,7 @@ SMODS.Consumable {
 	end,
 }
 
-     local conv_card = G.hand.highlighted[1]
-        G.E_MANAGER:add_event(Event({func = function()
-            play_sound('tarot1')
-            used_tarot:juice_up(0.3, 0.5)
-            return true end }))
-        
-        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.1,func = function()
-            conv_card:set_seal(self.ability.extra, nil, true)
-            return true end }))
-        
-        delay(0.5)
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() G.hand:unhighlight_all(); return true end }))
+     
 
 ----------------------------------------------
 ------------MOD CODE END----------------------
